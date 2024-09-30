f56b6d3990f474e1eb997594cb6b2d631c50608d
Thanapat Nawang <thanapattt.m@gmail.com>
2024-09-30 21:48:45 +0700

    add meta tag
    author
    description

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,11 @@
 @extends('layout')
+@section('desc', 'Watch movies for free at Pandora! Enjoy the latest releases and classic films in HD with no ads or hidden fees. Sign up now to access unlimited streaming on all devices. (This is a mini project for class work)')
 @section('titile', 'Home')
 @section('content')
     <div class="block text-lg">
         Hello
+        <div class="text-sm lg:text-2xl">
+            TEXTTTT
+        </div>
     </div>
 @endsection
